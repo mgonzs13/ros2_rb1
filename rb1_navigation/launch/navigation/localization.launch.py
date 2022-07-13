56691a2e7312f402022a2fd13424a901b32218ed
Irene González Fernández <igonzf06@estudiantes.unileon.es>
2022-07-13 10:18:42 +0200

    update humble version

--- rb1_navigation/launch/navigation/localization.launch.py
+++ rb1_navigation/launch/navigation/localization.launch.py
@@ -3,37 +3,72 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable
-from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, GroupAction, SetEnvironmentVariable
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch_ros.actions import LoadComposableNodes
 from launch_ros.actions import Node
+from launch_ros.descriptions import ComposableNode
 from nav2_common.launch import RewrittenYaml
 
 
 def generate_launch_description():
+    # Get the launch directory
     bringup_dir = get_package_share_directory("nav2_bringup")
-
-    initial_pose_x = LaunchConfiguration("initial_pose_x")
-    initial_pose_y = LaunchConfiguration("initial_pose_y")
-    initial_pose_z = LaunchConfiguration("initial_pose_z")
-    initial_pose_yaw = LaunchConfiguration("initial_pose_yaw")
 
     namespace = LaunchConfiguration("namespace")
     map_yaml_file = LaunchConfiguration("map")
     use_sim_time = LaunchConfiguration("use_sim_time")
     autostart = LaunchConfiguration("autostart")
     params_file = LaunchConfiguration("params_file")
+    use_composition = LaunchConfiguration("use_composition")
+    container_name = LaunchConfiguration("container_name")
+    use_respawn = LaunchConfiguration("use_respawn")
+    initial_pose_x = LaunchConfiguration("initial_pose_x")
+    initial_pose_x_cmd = DeclareLaunchArgument(
+        "initial_pose_x",
+        default_value="1.336",
+        description="Initial pose x")
+
+    initial_pose_y = LaunchConfiguration("initial_pose_y")
+    initial_pose_y_cmd = DeclareLaunchArgument(
+        "initial_pose_y",
+        default_value="6.544",
+        description="Initial pose y")
+
+    initial_pose_z = LaunchConfiguration("initial_pose_z")
+    initial_pose_z_cmd = DeclareLaunchArgument(
+        "initial_pose_z",
+        default_value="0.0",
+        description="Initial pose z")
+
+    initial_pose_yaw = LaunchConfiguration("initial_pose_yaw")
+    initial_pose_yaw_cmd = DeclareLaunchArgument(
+        "initial_pose_yaw",
+        default_value="0.0",
+        description="Initial pose yaw")
+
+
     lifecycle_nodes = ["map_server", "amcl"]
 
+    # Map fully qualified names to relative ones so the node"s namespace can be prepended.
+    # In case of the transforms (tf), currently, there doesn"t seem to be a better alternative
+    # https://github.com/ros/geometry2/issues/32
+    # https://github.com/ros/robot_state_publisher/pull/30
+    # TODO(orduno) Substitute with `PushNodeRemapping`
+    #              https://github.com/ros2/launch_ros/issues/56
     remappings = [("/tf", "tf"),
                   ("/tf_static", "tf_static")]
 
+    # Create our own temporary YAML files that include substitutions
     param_substitutions = {
         "use_sim_time": use_sim_time,
         "yaml_filename": map_yaml_file,
         "initial_pose.x": initial_pose_x,
         "initial_pose.y": initial_pose_y,
         "initial_pose.z": initial_pose_z,
-        "initial_pose.yaw": initial_pose_yaw}
+        "initial_pose.yaw": initial_pose_yaw
+        }
 
     configured_params = RewrittenYaml(
         source_file=params_file,
@@ -41,56 +76,125 @@ def generate_launch_description():
         param_rewrites=param_substitutions,
         convert_types=True)
 
-    return LaunchDescription([
+    stdout_linebuf_envvar = SetEnvironmentVariable(
+        "RCUTILS_LOGGING_BUFFERED_STREAM", "1")
 
-        SetEnvironmentVariable("RCUTILS_LOGGING_BUFFERED_STREAM", "1"),
+    declare_namespace_cmd = DeclareLaunchArgument(
+        "namespace",
+        default_value="",
+        description="Top-level namespace")
 
-        DeclareLaunchArgument(
-            "namespace", default_value="",
-            description="Top-level namespace"),
+    declare_map_yaml_cmd = DeclareLaunchArgument(
+        "map",
+        description="Full path to map yaml file to load")
 
-        DeclareLaunchArgument(
-            "map",
-            default_value=os.path.join(
-                bringup_dir, "maps", "turtlebot3_world.yaml"),
-            description="Full path to map yaml file to load"),
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        "use_sim_time",
+        default_value="false",
+        description="Use simulation (Gazebo) clock if true")
 
-        DeclareLaunchArgument(
-            "use_sim_time", default_value="False",
-            description="Use simulation (Gazebo) clock if True"),
+    declare_params_file_cmd = DeclareLaunchArgument(
+        "params_file",
+        default_value=os.path.join(bringup_dir, "params", "nav2_params.yaml"),
+        description="Full path to the ROS2 parameters file to use for all launched nodes")
 
-        DeclareLaunchArgument(
-            "autostart", default_value="True",
-            description="Automatically startup the nav2 stack"),
+    declare_autostart_cmd = DeclareLaunchArgument(
+        "autostart", default_value="true",
+        description="Automatically startup the nav2 stack")
 
-        DeclareLaunchArgument(
-            "params_file",
-            default_value=os.path.join(
-                bringup_dir, "config", "nav2_params.yaml"),
-            description="Full path to the ROS2 parameters file to use"),
+    declare_use_composition_cmd = DeclareLaunchArgument(
+        "use_composition", default_value="False",
+        description="Use composed bringup if True")
 
-        Node(
-            package="nav2_map_server",
-            executable="map_server",
-            name="map_server",
-            output="screen",
-            parameters=[configured_params],
-            remappings=remappings),
+    declare_container_name_cmd = DeclareLaunchArgument(
+        "container_name", default_value="nav2_container",
+        description="the name of conatiner that nodes will load in if use composition")
 
-        Node(
-            package="nav2_amcl",
-            executable="amcl",
-            name="amcl",
-            output="screen",
-            parameters=[configured_params],
-            remappings=remappings),
+    declare_use_respawn_cmd = DeclareLaunchArgument(
+        "use_respawn", default_value="False",
+        description="Whether to respawn if a node crashes. Applied when composition is disabled.")
 
-        Node(
-            package="nav2_lifecycle_manager",
-            executable="lifecycle_manager",
-            name="lifecycle_manager_localization",
-            output="screen",
-            parameters=[{"use_sim_time": use_sim_time},
-                        {"autostart": autostart},
-                        {"node_names": lifecycle_nodes}])
-    ])
+    load_nodes = GroupAction(
+        condition=IfCondition(PythonExpression(["not ", use_composition])),
+        actions=[
+            Node(
+                package="nav2_map_server",
+                executable="map_server",
+                name="map_server",
+                output="screen",
+                respawn=use_respawn,
+                respawn_delay=2.0,
+                parameters=[configured_params],
+                remappings=remappings),
+            Node(
+                package="nav2_amcl",
+                executable="amcl",
+                name="amcl",
+                output="screen",
+                respawn=use_respawn,
+                respawn_delay=2.0,
+                parameters=[configured_params],
+                remappings=remappings),
+            Node(
+                package="nav2_lifecycle_manager",
+                executable="lifecycle_manager",
+                name="lifecycle_manager_localization",
+                output="screen",
+                parameters=[{"use_sim_time": use_sim_time},
+                            {"autostart": autostart},
+                            {"node_names": lifecycle_nodes}])
+        ]
+    )
+
+    load_composable_nodes = LoadComposableNodes(
+        condition=IfCondition(use_composition),
+        target_container=container_name,
+        composable_node_descriptions=[
+            ComposableNode(
+                package="nav2_map_server",
+                plugin="nav2_map_server::MapServer",
+                name="map_server",
+                parameters=[configured_params],
+                remappings=remappings),
+            ComposableNode(
+                package="nav2_amcl",
+                plugin="nav2_amcl::AmclNode",
+                name="amcl",
+                parameters=[configured_params],
+                remappings=remappings),
+            ComposableNode(
+                package="nav2_lifecycle_manager",
+                plugin="nav2_lifecycle_manager::LifecycleManager",
+                name="lifecycle_manager_localization",
+                parameters=[{"use_sim_time": use_sim_time,
+                             "autostart": autostart,
+                             "node_names": lifecycle_nodes}]),
+        ],
+    )
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    # Set environment variables
+    ld.add_action(stdout_linebuf_envvar)
+
+    # Declare the launch options
+    ld.add_action(declare_namespace_cmd)
+    ld.add_action(declare_map_yaml_cmd)
+    ld.add_action(declare_use_sim_time_cmd)
+    ld.add_action(declare_params_file_cmd)
+    ld.add_action(declare_autostart_cmd)
+    ld.add_action(declare_use_composition_cmd)
+    ld.add_action(declare_container_name_cmd)
+    ld.add_action(declare_use_respawn_cmd)
+    ld.add_action(initial_pose_x_cmd)
+    ld.add_action(initial_pose_y_cmd)
+    ld.add_action(initial_pose_z_cmd)
+    ld.add_action(initial_pose_yaw_cmd)
+
+
+    # Add the actions to launch all of the localiztion nodes
+    ld.add_action(load_nodes)
+    ld.add_action(load_composable_nodes)
+
+    return ld
